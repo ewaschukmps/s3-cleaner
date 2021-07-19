c68fe216eaf16fd720ed95ab387b31838ddeeffc
Max Ewaschuk <ewaschukmps@gmail.com>
2021-07-18 22:56:05 -0500

    Adding logic to create s3 client using parameter or system credentials.

--- s3cleaner.py
+++ s3cleaner.py
@@ -2,16 +2,15 @@ import boto3
 import argparse
 
 
-def get_client(access_key_id):
-	
+def get_s3_client(access_key_id=None, secret_access_key=None):
+	if access_key_id and secret_access_key:
+		return boto3.client('s3', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+	else:
+		return boto3.client('s3')
 
 
-def main(access_key_id, secret_access_key):
-	if access_key_id:
-		print(f'ACCESS KEY ID: {access_key_id}')
-
-	if secret_access_key:
-		print(f'SECRET ACCESS KEY: {secret_access_key}')
+def main(access_key_id=None, secret_access_key=None):
+	client = get_s3_client(access_key_id, secret_access_key)
 
 
 if __name__ == "__main__":
@@ -20,4 +19,12 @@ if __name__ == "__main__":
 	parser.add_argument('--secret-access-key', default=None, type=str, required=False, help='(optional) Secret Access Key for the AWS profile.')
 	args = parser.parse_args()
 
-	main(args.access_key_id, args.secret_access_key)
+	access_key_id = args.access_key_id
+	secret_access_key = args.secret_access_key
+
+	if access_key_id and secret_access_key:
+		main(access_key_id, secret_access_key)
+	elif not access_key_id and not secret_access_key:
+		main()
+	else:
+		print('You must provide both the Access Key ID and Secret Access Key, or neither to use system credentials.')
